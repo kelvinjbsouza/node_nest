e9a76229f6347a77f5e56c6d33c79157554009d3
Kelvin Jhonnys <kelvinjbsouza@gmail.com>
2024-04-26 20:40:51 -0300

    Foram adicionados ao projeto o Youch e o Sentry para fins de tratamento de erros. Foi também adicionado o arquivo start_redis.sh que ao ser executado inicia o Redis

--- src/app.js
+++ src/app.js
@@ -1,26 +1,49 @@
 import "dotenv/config";
 
 import express from "express";
+import * as Sentry from "@sentry/node";
+import Youch from "youch";
+import "express-async-errors";
 
 // import authMiddleware from "./app/middlewares/auth";
 import routes from "./routes";
 
 import "./database";
 
+import sentryConfig from "./config/sentry";
+
 class App {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(Sentry.Handlers.tracingHandler());
     this.server.use(express.json());
     // this.server.use(authMiddleware);
   }
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === "development") {
+        const errors = await new Youch(err, req).toJSON();
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: "Internal server error" });
+    });
   }
 }
 
